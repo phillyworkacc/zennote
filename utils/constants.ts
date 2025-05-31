2967da0e6306998556b26a5cf379a7ec1c751c31
AyomiposiPhilip <ayomiposi.opadijo@gmail.com>
2025-05-31 16:10:27 +0100

    add appurl

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,3 @@
-export const appUrl = "http://localhost:3000"
+export const appUrl = "https://zennote-five.vercel.app"
 export const categories: TodoCategory[] = ['personal', 'work', 'school', 'family', 'friends', 'other']
 export const priorities: TodoPriority[] = ['high', 'medium', 'low']
